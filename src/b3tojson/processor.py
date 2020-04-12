8b8102bde6423bf480c724a9367ecb368cd2fc9b
Gerson Carlos <gerson.mtg@gmail.com>
2020-04-12 20:25:18 -0300

    Adding initial unittest and small fixes.

--- src/b3tojson/processor.py
+++ src/b3tojson/processor.py
@@ -1,5 +1,4 @@
 from b3tojson.data import Company, Stock
-from typing import Dict
 
 import jsonpickle
 
@@ -11,7 +10,7 @@ class FileHandle():
         self.in_file = in_file
         self.out_file = out_file
         self.companies = {}
-    
+
     def _process_raw_data(self, lines):
         lines = [line.strip() for line in lines]
         for line in lines:
@@ -28,13 +27,15 @@ class FileHandle():
                 bdi = line[18:21]
                 # 002: STD STOCK // 012: FII
                 if bdi == '002' or bdi == '012':
-                    self.companies[code].add_stock(Stock(line[2:14], bdi, line[21:81], line[133:143]))
-    
+                    self.companies[code].add_stock(Stock(line[2:14], bdi,
+                                                         line[21:81],
+                                                         line[133:143]))
+
     def _to_json(self):
         jsonpickle.set_preferred_backend('json')
         jsonpickle.set_encoder_options('json', ensure_ascii=False)
         return jsonpickle.encode(self.companies, unpicklable=False)
-    
+
     def analyze_file(self):
         try:
             with open(self.in_file, "r", encoding="utf-8") as fd:
@@ -48,7 +49,7 @@ class FileHandle():
             raise
         else:
             self._process_raw_data(lines)
-    
+
     def save_json(self):
         encoded = self._to_json()
 
